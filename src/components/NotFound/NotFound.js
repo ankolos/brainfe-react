9e3d1936901978b1038d5c11457ab8864b39dec6
Andrey <myemail.external@gmail.com>
2019-04-24 20:33:07 +0300

    up

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class NotFound extends Component {
+  constructor(props) {
+    super(props);
+    this.searchContent = [];
+    this.state = {
+      redirectToReferrer: false
+    };
+  }
+
+  componentWillMount() {
+    if (sessionStorage.getItem("userData")) {
+
+    }
+    else {
+      this.setState({ redirectToReferrer: true });
+    }
+  }
+
   render() {
+    if (this.state.redirectToReferrer) {
+      return (<Redirect to={'/login'} />)
+    }
     return (
       <div className="container">
         <div className="row justify-content-center align-items-center m-2">
