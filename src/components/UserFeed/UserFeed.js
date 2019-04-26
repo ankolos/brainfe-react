b0c49a1fbba06d2626f782dd4cbfdafd41867338
Andrey <myemail.external@gmail.com>
2019-04-26 16:06:48 +0300

    up

--- src/components/UserFeed/UserFeed.js
+++ src/components/UserFeed/UserFeed.js
@@ -1,53 +1,47 @@
 import React, { Component } from 'react';
-import Linkify from 'react-linkify';
 import TimeAgo from 'react-timeago';
-import './UserFeed.css';
 
 class UserFeed extends Component {
-
   constructor(props) {
     super(props);
-
-
   }
 
   render() {
-
-
     let userFeed = this
       .props
       .feedData
       .map(function (feedData, index) {
         return (
-            <div className="row border shadow m-2" key={index}>
-              <div className="col border">
-                <p>{this.props.name}</p>
+          <div className="row rounded shadow-lg mx-2 my-4" key={index}>
+            <div className="container">
+              <div className="row">
+                <div className="col p-2">
+                  <p className="font-weight-bold m-0">{this.props.name}</p>
+                </div>
               </div>
-              <div className="col border">
-                <p>{feedData.feed}</p>
+              <div className="row">
+                <div className="col p-2">
+                  <p className="m-0">{feedData.feed}</p>
+                </div>
               </div>
-              <div className="col border">
-                <div className="row">
-                  <div className="col">
-                    <TimeAgo date={this.props.convertTime(feedData.created)} />
-                  </div>
-                  <div className="col border">
-                    <button className="button secondary small" onClick={this.props.deleteUserFeed} data={feedData.feed_id} value={index} >  </button>
-                  </div>
+              <div className="row">
+                <div className="col p-2">
+                  <TimeAgo className="small" date={this.props.convertTime(feedData.created)} />
+                </div>
+                <div className="col p-2 text-right">
+                  <button className="btn btn-sm btn-outline-dark" onClick={this.props.deleteUserFeed} data={feedData.feed_id} value={index}>Delete</button>
                 </div>
               </div>
             </div>
+          </div>
         )
       }, this);
 
     return (
       <React.Fragment>
-              {userFeed}
-            </React.Fragment>
-
+        {userFeed}
+      </React.Fragment>
     );
   }
-
 }
-
 export default UserFeed;
