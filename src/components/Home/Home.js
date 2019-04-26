9bec576892a9d10e4da8549e75c79a035f30773a
Andrey <myemail.external@gmail.com>
2019-04-26 15:13:03 +0300

    up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,6 @@ import Navbar from '../../components/Navbar/Navbar';
 import { PostData } from '../../services/PostData';
 import UserFeed from '../../components/UserFeed/UserFeed';
 
-
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -16,11 +15,12 @@ class Home extends Component {
       redirectToReferrer: false
     };
     this.getUserFeed = this.getUserFeed.bind(this);
-    this.feedUpdate = this.feedUpdate.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.deleteFeed = this.deleteFeed.bind(this);
-    this.deleteFeedAction = this.deleteFeedAction.bind(this);
+    this.updateUserFeed = this.updateUserFeed.bind(this);
+    this.deleteUserFeed = this.deleteUserFeed.bind(this);
     this.convertTime = this.convertTime.bind(this);
+
+    this.onChange = this.onChange.bind(this);
+    //this.deleteFeed = this.deleteFeed.bind(this);
   }
 
   componentWillMount() {
@@ -32,75 +32,100 @@ class Home extends Component {
     }
   }
 
-  feedUpdate(e) {
-    e.preventDefault();
+  /**
+   * Get user feed data from session storage
+   */
+  getUserFeed() {
     let data = JSON.parse(sessionStorage.getItem("userData"));
-    let postData = { user_id: data.userData.user_id, token: data.userData.token, feed: this.state.userFeed };
-    if (this.state.userFeed) {
-      PostData('feedUpdate', postData).then((result) => {
-        let responseJson = result;
-        let K = [responseJson.feedData].concat(this.state.data);
-        this.setState({ data: K, userFeed: '' });
-      });
-    }
-  }
+    this.setState({ name: data.userData.name });
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token
+    };
 
-  convertTime(created) {
-    let date = new Date(created * 1000);
-    return date;
-  }
-
-  deleteFeedAction(e) {
-    console.log("HI");
-    let updateIndex = e.target.getAttribute('value');
-    let feed_id = e.target.getAttribute('data');
-
-    let data = JSON.parse(sessionStorage.getItem("userData"));
-
-    let postData = { user_id: data.userData.user_id, token: data.userData.token, feed_id: feed_id };
     if (postData) {
-      PostData('feedDelete', postData).then((result) => {
-        this
-          .state
-          .data.splice(updateIndex, 1);
+      PostData('feed', postData).then((result) => {
         this.setState({
-          data: this
-            .state
-            .data
+          data: result.feedData
         });
       });
     }
   }
 
-  deleteFeed(e) {
-    confirmAlert({
-      title: '',
-      message: 'Are you sure?',
-      childrenElement: () => '',
-      confirmLabel: 'Delete',
-      cancelLabel: 'Cancel',
-      onConfirm: () => this.deleteFeedAction(e),
-      onCancel: () => '',
-    })
-  }
-
-  getUserFeed() {
+  /**
+   * Update user feed at backend
+   * @param {*} e 
+   */
+  updateUserFeed(e) {
+    e.preventDefault();
     let data = JSON.parse(sessionStorage.getItem("userData"));
-    this.setState({ name: data.userData.name });
-    let postData = { user_id: data.userData.user_id, token: data.userData.token };
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token,
+      feed: this.state.userFeed
+    };
 
-    if (data) {
-      PostData('feed', postData).then((result) => {
-        let responseJson = result;
-        this.setState({ data: responseJson.feedData });
-        console.log(this.state);
+    if (this.state.userFeed) {
+      PostData('feedUpdate', postData).then((result) => {
+        let feedData = [result.feedData].concat(this.state.data);
+        console.log(result.feedData);
+        this.setState({
+          data: feedData,
+          userFeed: ''
+        });
       });
     }
+  }
+
+  /**
+   * Delete user feed at backend
+   * @param {*} e 
+   */
+  deleteUserFeed(e) {
+    let updateIndex = e.target.getAttribute('value');
+    let feed_id = e.target.getAttribute('data');
+
+    let data = JSON.parse(sessionStorage.getItem("userData"));
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token,
+      feed_id: feed_id
+    };
+
+    if (postData) {
+      PostData('feedDelete', postData).then((result) => {
+        this.state.data.splice(updateIndex, 1);
+        this.setState({
+          data: this.state.data
+        });
+      });
+    }
+  }
+
+  /**
+   * Convert millisecond to second
+   * @param {*} created 
+   */
+  convertTime(created) {
+    let date = new Date(created * 1000);
+    return date;
   }
 
   onChange(e) {
     this.setState({ userFeed: e.target.value });
   }
+
+  // deleteFeed(e) {
+  //   confirmAlert({
+  //     title: '',
+  //     message: 'Are you sure?',
+  //     childrenElement: () => '',
+  //     confirmLabel: 'Delete',
+  //     cancelLabel: 'Cancel',
+  //     onConfirm: () => this.deleteFeedAction(e),
+  //     onCancel: () => '',
+  //   })
+  // }
 
   render() {
     if (this.state.redirectToReferrer) {
@@ -110,16 +135,16 @@ class Home extends Component {
       <React.Fragment>
         <Navbar />
         <div className="col border border-dark rounded mt-2 mr-2 mb-2 p-0">
-          <div className="container">
-            <div className="row justify-content-left p-2">
-              <form className="form-inline" onSubmit={this.feedUpdate}>
-                <div class="form-group mr-2">
+          <div className="container p-0">
+            <div className="row justify-content-left m-2">
+              <form className="form-inline" onSubmit={this.updateUserFeed}>
+                <div className="form-group mr-2">
                   <textarea className="form-control" rows="1" type="text" id="userFeed" name="userFeed" placeholder="What's up?" value={this.state.userFeed} onChange={this.onChange} />
                 </div>
                 <button type="submit" className="btn btn-outline-dark">Post</button>
               </form>
             </div>
-            <UserFeed feedData={this.state.data} deleteFeedAction={this.deleteFeedAction} convertTime={this.convertTime} name={this.state.name} />
+            <UserFeed feedData={this.state.data} deleteUserFeed={this.deleteUserFeed} convertTime={this.convertTime} name={this.state.name} />
           </div>
         </div>
       </React.Fragment>
